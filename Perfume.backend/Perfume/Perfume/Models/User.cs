44c4f4cfa7eeb62f8e66dae35679ab265f2ce315
berkwn <berkand180@gmail.com>
2026-03-15 13:46:52 +0300

    kontrol.

--- Perfume.backend/Perfume/Perfume/Models/User.cs
+++ Perfume.backend/Perfume/Perfume/Models/User.cs
@@ -6,7 +6,7 @@
         public string Username { get; set; }
         public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
+        public byte[]? PasswordSalt { get; set; }
         public string Role { get; set; } = "User";
         public DateTime CreatedAt { get; set; }
         public string RefreshToken { get; set; } = string.Empty;
